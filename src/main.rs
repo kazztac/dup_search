bf9bf9b3a50d3faa16369ac0dccc093bc78969b4
kazztac <42577573+kazztac@users.noreply.github.com>
2020-10-27 22:33:03 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //TODO: Open a fixed file.
+    //TODO: Calculate a hash value by MD5 algorithm.
+    //TODO: Read args from command line.
+    //TODO: Open a file specified in args.
+    //TODO: Calculate a hash value by original algorithm.
+    //TODO: Search files recursively from a folder specified in args.
+    //TODO: Hold hash values for searched files.
+    //TODO: Output all duplicated files.
+    //TODO: Output result as a specified file format.
+    //TODO: Use async for all file-io.
+}
