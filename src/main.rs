9e368c5392aeec27d90c88712add000457b82041
kazztac <42577573+kazztac@users.noreply.github.com>
2020-11-24 23:35:09 +0900

    Change the MD5 crate to use implementing RustCrypt crate one.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,19 @@ use async_recursion::async_recursion;
 use async_std::fs::{read_dir, File};
 use async_std::io::BufReader;
 use async_std::{prelude::*, task};
+use digest::Digest;
+use hex::ToHex;
 use multimap::MultiMap;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-#[allow(dead_code)]
-fn sha256(b: &Vec<u8>) -> String {
-    use sha2::Digest;
-    format!("{:x}", sha2::Sha256::digest(b))
-}
-
-fn md5(b: &Vec<u8>) -> String {
-    format!("{:x}", md5::compute(b))
+fn hash<D>(b: &Vec<u8>) -> String
+where
+    D: Digest,
+{
+    let mut digest = D::new();
+    digest.update(b);
+    digest.finalize().as_slice().encode_hex()
 }
 
 async fn calculate_hash_of(file_path: &str) -> Result<String> {
@@ -21,9 +22,7 @@ async fn calculate_hash_of(file_path: &str) -> Result<String> {
     let mut buf_read = BufReader::new(file);
     let mut buf = Vec::new();
     let _read_length = buf_read.read_to_end(&mut buf).await?;
-    let algorithm = md5;
-    //let algorithm = sha256;
-    let hashed_value = algorithm(&buf);
+    let hashed_value = hash::<md5::Md5>(&buf);
     Ok(hashed_value)
 }
 
@@ -80,6 +79,7 @@ fn main() {
     task::block_on(async { run().await.unwrap() });
     eprintln!("--- Finish ---");
 
+    //TODO: Add the Blake3 crate.
     //TODO: Read args from command line.
     //TODO: Open a file specified in args.
     //TODO: Search files recursively from a folder specified in args.
