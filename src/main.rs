d7ab5c13300b192171218f8616bec266a15fbc72
kazztac <42577573+kazztac@users.noreply.github.com>
2020-12-12 09:34:16 +0900

    Updated

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,26 @@ async fn print_progress(mut rx: UnboundedReceiver<usize>, total: usize) {
     let mut progress_count = 0;
     while let Some(recv_count) = rx.next().await {
         progress_count += recv_count;
-        //print!("\r{:5} / {:5}", progress_count, total);
-        info!("\r{:5} / {:5}", progress_count, total);
+        print!("\r{:5} / {:5}", progress_count, total);
     }
-    //println!();
-    info!("\n");
+    println!();
 }
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    pretty_env_logger::init();
+    pretty_env_logger::init_timed();
+    info!("--- Start ---");
+    let total_start = std::time::Instant::now();
+    info!("Start: Arg setup");
+    let start = std::time::Instant::now();
     let args = dup_search::args::parse_args().unwrap();
     let hash_param = HashParam {
         algorithm: args.hash_algorithm(),
         buf_size: 1024 * 1024,
     };
+    info!("Finish: {:5}ms", start.elapsed().as_millis());
+    info!("Start: File search");
+    let start = std::time::Instant::now();
     let file_path_list = get_file_path_list_in(args.directory().to_string()).await?;
     let progress_sender = if args.is_verbose() {
         let (tx, rx) = mpsc::unbounded();
@@ -35,15 +40,23 @@ async fn main() -> Result<()> {
     } else {
         None
     };
+    info!("Finish: {:5}ms", start.elapsed().as_millis());
+    info!("Start: Calcurating hashes");
+    let start = std::time::Instant::now();
     let hash_files = calcurate_hashes_of(&file_path_list, &hash_param, progress_sender)
         .await
         .unwrap();
-    let output = match args.output_format() {
+    info!("Finish: {:5}ms", start.elapsed().as_millis());
+    info!("Start Output");
+    let start = std::time::Instant::now();
+    let _output = match args.output_format() {
         OutputFormat::JSON => serde_json::to_string(&hash_files).unwrap(),
         OutputFormat::YAML => serde_yaml::to_string(&hash_files).unwrap(),
         OutputFormat::TOML => toml::to_string(&hash_files).unwrap(),
     };
-    println!("{}", &output);
+    //println!("{}", &_output);
+    info!("Finish: {:5}ms", start.elapsed().as_millis());
+    info!("--- Finish: {:5}ms ---", total_start.elapsed().as_millis());
     Ok(())
 
     //TODO: Performance optimization.
