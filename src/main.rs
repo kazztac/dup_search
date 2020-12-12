452ae82399dfcfe9e241eaf46566d2c0eda5c70d
kazztac <42577573+kazztac@users.noreply.github.com>
2020-12-13 00:01:38 +0900

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use async_std::task;
 use dup_search::args::OutputFormat;
 use dup_search::hash::{calcurate_hashes_of, HashParam};
-use dup_search::util::get_file_path_list_in;
+//use dup_search::util::get_file_path_list_in;
+use dup_search::util::get_file_path_list_in2;
 use dup_search::Result;
 use futures::channel::mpsc;
 use futures::stream::StreamExt;
-use mpsc::UnboundedReceiver;
 use log::info;
+use mpsc::UnboundedReceiver;
 
 async fn print_progress(mut rx: UnboundedReceiver<usize>, total: usize) {
     let mut progress_count = 0;
@@ -32,7 +33,20 @@ async fn main() -> Result<()> {
     info!("Finish: {:5}ms", start.elapsed().as_millis());
     info!("Start: File search");
     let start = std::time::Instant::now();
-    let file_path_list = get_file_path_list_in(args.directory().to_string()).await?;
+    //    let file_path_list = get_file_path_list_in(args.directory().to_string()).await?;
+    let (mut tx, mut rx) = mpsc::unbounded();
+    let cloned_folder_path = args.directory().to_string();
+    task::spawn(async move {
+        let _ignore = get_file_path_list_in2(&cloned_folder_path, &mut tx).await;
+    });
+    let mut file_path_list = vec![];
+    while let Some(msg) = rx.next().await {
+        let file_path = msg?;
+        file_path_list.push(file_path);
+    }
+    info!("Finish: {:5}ms", start.elapsed().as_millis());
+    info!("Start: Calcurating hashes");
+    let start = std::time::Instant::now();
     let progress_sender = if args.is_verbose() {
         let (tx, rx) = mpsc::unbounded();
         task::spawn(print_progress(rx, file_path_list.len()));
@@ -40,9 +54,6 @@ async fn main() -> Result<()> {
     } else {
         None
     };
-    info!("Finish: {:5}ms", start.elapsed().as_millis());
-    info!("Start: Calcurating hashes");
-    let start = std::time::Instant::now();
     let hash_files = calcurate_hashes_of(&file_path_list, &hash_param, progress_sender)
         .await
         .unwrap();
@@ -54,14 +65,14 @@ async fn main() -> Result<()> {
         OutputFormat::YAML => serde_yaml::to_string(&hash_files).unwrap(),
         OutputFormat::TOML => toml::to_string(&hash_files).unwrap(),
     };
-    //println!("{}", &_output);
+    println!("{}", &_output);
     info!("Finish: {:5}ms", start.elapsed().as_millis());
     info!("--- Finish: {:5}ms ---", total_start.elapsed().as_millis());
     Ok(())
 
     //TODO: Performance optimization.
     //  done: 1. The first task will search the file paths and send it by a channel.
-    //  2. If the task find a directory, it spawns a new task searches there.
+    //  done 2. If the task find a directory, it spawns a new task searches there.
     //  3. If the channel notifies new message, it spawns a new task that calcurates the hash of
     //     the file until the number of the task will be the ulimit size.
     //  4. The task spawned on #3 retrieves the file path from the storage that be shared each
